8cbf66dbc46117b181712c66a9059b68c57165d5
Joseph Magen <jmagen@redhat.com>
2014-11-09 17:00:27 +0200

    squash this commit

--- app/controllers/configure/new-environment.js
+++ app/controllers/configure/new-environment.js
@@ -2,6 +2,10 @@ import Ember from 'ember';
 
 export default Ember.ObjectController.extend({
   fields: {},
+  labelName: function() {
+    return this.get('fields.name');
+  }.property('fields.name'),
+
   actions: {
     cancel: function() {
       this.transitionToRoute('configure');
