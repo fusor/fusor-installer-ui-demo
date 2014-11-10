af780d2b04cb334ecd1eedf22c546c8925b658d3
Joseph Magen <jmagen@redhat.com>
2014-11-10 11:04:59 +0200

    squash this commit

--- app/controllers/rhci.js
+++ app/controllers/rhci.js
@@ -1,8 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.ObjectController.extend({
-    'isSatellite': true,
-    'isRhev': true,
-    'isOpenStack': true,
-    'isCloudForms': true
+    isSatellite: true,
+    isRhev: true,
+    isOpenStack: true,
+    isCloudFormsOnRhev: true,
+    isCloudFormsOnOpenstack: false,
+    toggleCF: function(){
+        if (this.get('isCloudFormsOnOpenstack')) {
+          this.set('isCloudFormsOnRhev', false);
+        }
+    }.observes('isCloudFormsOnOpenstack'),
+    watchFoo222: function(){
+        if (this.get('isCloudFormsOnRhev')) {
+          this.set('isCloudFormsOnOpenstack', false);
+        }
+    }.observes('isCloudFormsOnRhev'),
+
+
 });
