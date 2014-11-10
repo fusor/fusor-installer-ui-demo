4013b95b1e816e73b895e30f87871c12a9057055
Joseph Magen <jmagen@redhat.com>
2014-11-10 16:31:30 +0200

    squash this commit

--- app/routes/hypervisor/new-host.js
+++ app/routes/hypervisor/new-host.js
@@ -1,4 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  setupController: function(controller, model) {
+    controller.set('model', model);
+    controller.set('organizations', this.store.find('organization'));
+    controller.set('environments', this.store.find('environment'));
+  }
 });
