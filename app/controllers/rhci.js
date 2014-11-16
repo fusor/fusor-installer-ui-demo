e6fc06c26a0363ca209f8174531eb5efe30569de
Joseph Magen <jmagen@redhat.com>
2014-11-16 15:53:38 +0200

    rhci install

--- app/controllers/rhci.js
+++ app/controllers/rhci.js
@@ -3,6 +3,9 @@ import Ember from 'ember';
 export default Ember.ObjectController.extend({
   isRhev: true,
   isOpenStack: true,
-  isCloudForms: true
+  isCloudForms: true,
 
+  disableNext: function () {
+    return (!(this.get('isRhev') || this.get('isOpenStack') || this.get('isCloudForms')));
+  }.property('isRhev', 'isOpenStack', 'isCloudForms')
 });
