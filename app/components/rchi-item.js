1d92e8248aa09246f528081cf2db3f3b45c0232d
Joseph Magen <jmagen@redhat.com>
2014-11-16 15:31:55 +0200

    squash this commit

--- app/components/rchi-item.js
+++ app/components/rchi-item.js
@@ -1,11 +1,38 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  classNameBindings: ['isChecked:rhci-item-selected:rhci-item-deselected'],
+  classNames: ['rhci-item'],
+  classNameBindings: ['isChecked:rhci-item-selected:rhci-item-deselected', 'isHover:rhci-item-hover'],
 
-  click: function(event) {
+  click: function() {
     this.set('isChecked', this.toggleProperty('isChecked'));
+    // if (this.get('isChecked')) {
+    //   this.set('isHover', false);
+    // }
   },
 
+  isHover: false,
+
+  showMsgToSelect: function() {
+    return ( (this.get('isHover')) && (!(this.get('isChecked'))) );
+  }.property('isHover', 'isChecked'),
+
+  showMsgToDeselect: function() {
+    return ( (this.get('isHover')) && (this.get('isChecked')) );
+  }.property('isHover', 'isChecked'),
+
+  mouseEnter: function(){
+    this.set('isHover', true);
+  },
+
+  mouseLeave: function(){
+    this.set('isHover', false);
+  },
+
+  // actions: {
+  //   toggleHover: function() {
+  //     this.set('isHover', this.toggleProperty('isHover'));
+  //   }
+  // }
 
 });
