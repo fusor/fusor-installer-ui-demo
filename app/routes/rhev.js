f225057c152477f09df48ba0198942a2a2ac52a6
Joseph Magen <jmagen@redhat.com>
2014-11-17 17:35:27 +0200

    model note

--- app/routes/rhev.js
+++ app/routes/rhev.js
@@ -3,6 +3,10 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   setupController: function(controller, model) {
     controller.set('model', model);
-    this.controllerFor('side-menu').set('uxNotes', 'RHEV notes');
+  },
+
+  activate: function() {
+    this.controllerFor('side-menu').set('uxNotes', 'RHEV notes 3333333');
   }
+
 });
--- app/routes/where-install.js
+++ app/routes/where-install.js
@@ -1,4 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  activate: function() {
+    this.controllerFor('side-menu').set('uxNotes', 'where install \
+    adfadf \
+    **dfadf** notes');
+  }
 });
