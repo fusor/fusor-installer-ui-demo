3829d7a3c790d1d1055bc0160cc7645f31d4fbe5
Joseph Magen <jmagen@redhat.com>
2014-11-18 13:07:57 +0200

    squash this commit

--- app/controllers/storage.js
+++ app/controllers/storage.js
@@ -3,6 +3,6 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   storageType: 'NFS',
   isNFS: function() {
-    return (this.storageType == 'NFS');
+    return (this.storageType === 'NFS');
   }.property('storageType')
 });
