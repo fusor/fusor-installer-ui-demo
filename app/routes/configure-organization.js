d239b2c92d38c99e9f7d26d3f76d81b1318c0d26
Joseph Magen <jmagen@redhat.com>
2014-11-18 11:58:23 +0200

    squash this commit

--- app/routes/configure-organization.js
+++ app/routes/configure-organization.js
@@ -5,5 +5,11 @@ export default Ember.Route.extend({
     controller.set('model', model);
     controller.set('organizations', this.store.find('organization'));
   },
+  activate: function() {
+    this.controllerFor('side-menu').set('etherpadName', '41'); //route-satellite
+  },
 
+  deactivate: function() {
+    this.controllerFor('side-menu').set('etherpadName', '');
+  }
 });
