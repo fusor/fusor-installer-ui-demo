ab76d9c20905e3a58f56f2c67dc37f7b367e309b
Joseph Magen <jmagen@redhat.com>
2014-11-18 15:27:20 +0200

    squash this commit

--- app/controllers/side-menu.js
+++ app/controllers/side-menu.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  showSideMenu: true,
+  showSideMenu: false,
 
   uxDefaultNote: 'Note: Please write notes on [etherpad](http://rhci.pad.engineering.redhat.com/wireframe-mtg-10-30-2014), since this pad is ready-only and will note save anything.\n\n',
   uxHeaderNote: '\n\n\n**UX Notes / Specs** for this route\n\n',
