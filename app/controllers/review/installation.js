0efb25e23097c391b225f8927cb3bb0a2ffbe8ac
Joseph Magen <jmagen@redhat.com>
2014-11-27 12:17:35 +0200

    review installation

--- app/controllers/review/installation.js
+++ app/controllers/review/installation.js
@@ -1,7 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  needs: ['rhci'],
+  needs: ['application','rhci', 'deployment', 'satellite', "satellite/index", 'configure-organization', 'configure-environment', 'rhev-setup'],
+
+  nameDeployment: Ember.computed.alias("controllers.satellite/index.name"),
+  selectedOrganization: Ember.computed.alias("controllers.configure-organization.selectedOrganzation"),
+  selectedEnvironment: Ember.computed.alias("controllers.configure-organization.selectedEnvironment"),
+  rhevSetup: Ember.computed.alias("controllers.rhev-setup.rhevSetup"),
+  oVirtHostedtype: function() {
+    if (this.get('rhevSetup') === 'selfhost') {
+      return "Self Hosted";
+    } else {
+      return "Host + Engine";
+    }
+  }.property('rhevSetup'),
 
   nameRHCI: Ember.computed.alias("controllers.rhci.nameRHCI"),
   nameRhev: Ember.computed.alias("controllers.rhci.nameRhev"),
