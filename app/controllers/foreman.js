574eb88226dc43af03cd6f9c2688c8137cf45b67
Joseph Magen <jmagen@redhat.com>
2014-12-01 12:24:12 +0200

    installer options

--- app/controllers/foreman.js
+++ app/controllers/foreman.js
@@ -2,10 +2,15 @@ import Ember from 'ember';
 
 export default Ember.ObjectController.extend({
 
-  app_root: Ember.computed.alias("this.foreman.app_root"),
-  authentication: Ember.computed.alias("this.foreman.authentication"),
-  configure_epel_repo: Ember.computed.alias("this.foreman.configure_epel_repo"),
-  db_adapter: Ember.computed.alias("this.foreman.db_adapter"),
-  db_username: Ember.computed.alias("this.foreman.db_username"),
+  dbTypes: ["postgresql", "mysql", 'sqlite3'],
+  repoTypes: ["stable", "rc", "nightly"],
+
+  // app_root: Ember.computed.alias("this.foreman.app_root"),
+  // authentication: Ember.computed.alias("this.foreman.authentication"),
+  // configure_epel_repo: Ember.computed.alias("this.foreman.configure_epel_repo"),
+  // configure_scl_repo: Ember.computed.alias("this.foreman.configure_scl_repo"),
+  // db_adapter: Ember.computed.alias("this.foreman.db_adapter"),
+  // db_username: Ember.computed.alias("this.foreman.db_username"),
+  // custom_repo: Ember.computed.alias("this.foreman.custom_repo"),
 
 });
